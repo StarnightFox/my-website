2dc41d6a8d8592e14114ee6095fc54e58963e79f
Scarlette Fox <lexidahusky.102@gmail.com>
2023-06-06 17:59:52 +0100

    fix: wasnt using base url properly

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.base),
   routes: [
     {
       path: '/',
